4f7ccc688af64e6c4277c764bec6610a8eafd3c1
Michael McPherson <mike.a.mcpherson@gmail.com>
2024-08-18 22:21:49 -0600

    final code

--- csc500_mod1_CT.py
+++ csc500_mod1_CT.py
@@ -4,10 +4,10 @@
 ##### Dr Jessica Schwartz 
 
 # Part 1.
-## Write a Python Program to find addition and subtraction of two numbers.
-### - Ask the user to input the two numbers (num1 & num2.)
-### - Given those two numbers, add them together to find the output.
-### - Also, subtract the two numbers to find the output.
+"""Write a Python Program to find addition and subtraction of two numbers.
+- Ask the user to input the two numbers (num1 & num2.)
+- Given those two numbers, add them together to find the output.
+- Also, subtract the two numbers to find the output."""
 
 # Part1: Code
 # Function to add two numbers, 'the sum fiunction'.
@@ -32,12 +32,12 @@ print(f"The sum of {num1} and {num2} is {sum:,.5f}!")
 print(f"The difference of {num1} and {num2} is {difference:,.5f}")
 
 # Part 2.
-#### Write a Python program to find the multiplication and division of two numbers.
-##### - Ask the user to input two numbers (num1 and num2). 
-# Given those two numbers, multiply them together to find the output.
-#  Also, divide num1/num2 to find the output.
-##### - Compile and submit your pseudocode, source code, and screenshots of the application executing the code from parts 1 and 2,
-#  the results and GIT repository in a single document (Word is preferred).
+"""Write a Python program to find the multiplication and division of two numbers.
+- Ask the user to input two numbers (num1 and num2). 
+- Given those two numbers, multiply them together to find the output.
+- Also, divide num1/num2 to find the output.
+- Compile and submit your pseudocode, source code, and screenshots of the application executing the code from parts 1 and 2,
+the results and GIT repository in a single document (Word is preferred)."""
 
 # Part1: Code
 
@@ -54,8 +54,8 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 # calculate the sum and difference functions
-multiply= multiply(num1, num2)
-divide= divide(num1, num2)
+product= multiply(num1, num2)
+quotient= divide(num1, num2)
 
 print(f"The multiple of {num1} by {num2} is {multiply:,.5f}!")
 
